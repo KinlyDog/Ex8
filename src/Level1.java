cb0458301c8cad546da2a67aa155ec61bef7610e
Andrey Otryaskin <kinlydog@gmail.com>
2022-12-01 02:40:40 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -2,31 +2,29 @@ public class Level1 {
     public static int light(int time, int red, int green) {
         int sum = red + green;
         int count = time;
-        int TrafficLightColor = 0;
+        int trafficLightColor = 0;
 
         if (time > sum) {
             count = time % sum;
         }
 
         if (count < red) {
-            TrafficLightColor = red - count;
+            trafficLightColor = red - count;
         }
 
-        return TrafficLightColor;
+        return trafficLightColor;
     }
 
     public static int Unmanned(int l, int n, int[][] track) {
         int car = 0;
-        int x = 0;
+        int trafficLight = 0;
 
         for (int i = 1; i <= l; i++) {
             car++;
 
-            if (i == track[x][0]) {
-                car += light(car, track[x][1], track[x][2]);
-                x = x < n - 1 ?
-                        x + 1 :
-                        x;
+            if (i == track[trafficLight][0]) {
+                car += light(car, track[trafficLight][1], track[trafficLight][2]);
+                trafficLight = trafficLight < n - 1 ? trafficLight + 1 : trafficLight;
             }
         }
 
